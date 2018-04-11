92133dfa3e868b8ed4718958699edb017fd9a8c8
Luke Hindman <lukehindman@boisestate.edu>
2018-04-10 18:15:39 -0600

    Added rewritting version of CounterPanel with comments

--- cs121/FancyPushCounter/CounterPanel.java
+++ cs121/FancyPushCounter/CounterPanel.java
@@ -7,39 +7,54 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+/**
+ * In class example of creating a reusable CounterPanel
+ * @author lhindman
+ *
+ */
 public class CounterPanel extends JPanel {
 	
 	/* Instance Variables */
 	private JButton upButton, downButton;
 	private JLabel displayLabel;
-	private int value;
+	private int count;
 	
 	/* Constructor */
-	
 	public CounterPanel() {
-		this.value = 0;
+		this.count = 0;
 		
+		/* Create the shared listener for the up and down buttons */
 		CounterButtonListener cbl = new CounterButtonListener();
 		
+		/* Create the GUI components and adjust the alignment */
 		upButton = new JButton("Up");
 		upButton.setAlignmentX(Component.CENTER_ALIGNMENT);
 		upButton.addActionListener(cbl);
-		
 		
 		downButton = new JButton("Down");
 		downButton.setAlignmentX(Component.CENTER_ALIGNMENT);
 		downButton.addActionListener(cbl);
 		
-		displayLabel = new JLabel(Integer.toString(this.value));
+		displayLabel = new JLabel( Integer.toString(count));
 		displayLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
 		
-		/* Chnge default layout from FlowLayout to BoxLayout */
+		
+		/* Change layout from FlowLayout to BoxLayout and add components */
 		this.setLayout(new BoxLayout(this,BoxLayout.Y_AXIS));
+		
+		/* Add the GUI components to this panel */
 		this.add(upButton);
 		this.add(displayLabel);
 		this.add(downButton);
+		
 	}
 	
+	/**
+	 * Internal button listener to update the click count 
+	 *    and the text of the displayLabel
+	 * @author lhindman
+	 *
+	 */
 	private class CounterButtonListener implements ActionListener {
 
 		@Override
@@ -47,15 +62,18 @@ public class CounterPanel extends JPanel {
 			JButton b = (JButton) e.getSource();
 			
 			if (b == upButton) {
-				value++;	
+				count++;
 			} else {
-				value--;
+				count--;
 			}
-			displayLabel.setText(Integer.toString(value));
+			
+			displayLabel.setText(Integer.toString(count));
 			
 		}
 		
 	}
+	
+	
 	
 	
 
