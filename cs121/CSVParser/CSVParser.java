079f63767ed8376ed99cad5c83bb27b967e05c08
Luke Hindman <lukehindman@boisestate.edu>
2019-10-24 11:24:32 -0600

    Added conditional to check if file exists

--- cs121/CSVParser/CSVParser.java
+++ cs121/CSVParser/CSVParser.java
@@ -7,50 +7,53 @@ import java.util.Scanner;
 public class CSVParser {
 
 	public static void main(String[] args) {
-		
+
 		/* 1. Create a new File object */
 		File bobsFile = new File("jimmy_buffett-collection.csv");
-		
-		/* 2. Create a Scanner to read the file */
-		try {
-			Scanner bobsScanner = new Scanner(bobsFile);
-			
-			/* 3. Create a loop to read each line from the Scanner */
-			while(bobsScanner.hasNextLine()) {
-				String line = bobsScanner.nextLine();
-			
-				/* 4. Print each line */
-				System.out.println("Original Line: " + line);
-				
-				/* 5. Print out each word on its own line */
-				Scanner bobsLineScanner = new Scanner (line);
-				bobsLineScanner.useDelimiter(",");
-				
-				/* 6. Extract the individual fields from each line */
-				String artist = bobsLineScanner.next();
-				String album = bobsLineScanner.next();
-				String title = bobsLineScanner.next();
-				String duration = bobsLineScanner.next();
-				
-				/* 7. Print each song */
-				System.out.println("**************************");
-				System.out.println("Artist:" + artist);
-				System.out.println("Album:" + album);
-				System.out.println("Title: " + title);
-				System.out.println("Duration: " + duration);
-				System.out.println("**************************");
-				
-			}
-			
-		} catch (FileNotFoundException e) {
-			System.out.println("Unable to open file: " + bobsFile);
-			System.exit(1);
-		}
-		
-		
 
-		
-		
+		/* 1.5 Check if file exists */
+		if (bobsFile.exists() && bobsFile.isFile()) {
+
+			/* 2. Create a Scanner to read the file */
+			try {
+				Scanner bobsScanner = new Scanner(bobsFile);
+
+				/* 3. Create a loop to read each line from the Scanner */
+				while(bobsScanner.hasNextLine()) {
+					String line = bobsScanner.nextLine();
+
+					/* 4. Print each line */
+					System.out.println("Original Line: " + line);
+
+					/* 5. Print out each word on its own line */
+					Scanner bobsLineScanner = new Scanner (line);
+					bobsLineScanner.useDelimiter(",");
+
+					/* 6. Extract the individual fields from each line */
+					String artist = bobsLineScanner.next();
+					String album = bobsLineScanner.next();
+					String title = bobsLineScanner.next();
+					String duration = bobsLineScanner.next();
+
+					/* 7. Print each song */
+					System.out.println("**************************");
+					System.out.println("Artist:" + artist);
+					System.out.println("Album:" + album);
+					System.out.println("Title: " + title);
+					System.out.println("Duration: " + duration);
+					System.out.println("**************************");
+
+				}
+
+			} catch (FileNotFoundException e) {
+				System.out.println("Unable to open file: " + bobsFile);
+				System.exit(1);
+			}
+
+
+
+		}	
+
 
 	}
 
