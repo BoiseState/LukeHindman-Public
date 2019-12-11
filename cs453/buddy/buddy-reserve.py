b539025b6b7676a26c19daf0b9fce6e82682fe12
Luke Hindman <lukehindman@boisestate.edu>
2019-12-11 09:51:16 -0700

    Updated example to add comment about making the initial call to reserve include the sizeof the block header

--- cs453/buddy/buddy-reserve.py
+++ cs453/buddy/buddy-reserve.py
@@ -1,9 +1,16 @@
 # reserve - finds and reserves a block of 2^k bytes, or reports failure, using the
 # organization of the buddy system as described in Kunth
+#
+# NOTE:  The size passed to the 'initial' call of reservice should include
+#          the number of bytes requested from the user + sizeof(struct block_header).
+#          This ensures that the size of the block that is returned by reserve
+#          is large enough to hold both the amount of space requested by the user
+#          and the block header.
+#
 # @param size The size in bytes requested
 # @return pointer to reserved block matching request or None
 def reserve(size):
-	k = get_upper_k(size + sizeof(struct block_header))
+	k = get_upper_k(size)
 	m = pool.lgsize
 	j = k
 	# R1 - Find nearest available block
